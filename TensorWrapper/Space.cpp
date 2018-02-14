d75273d3c83215122884b08fd89bb53eed3797be
ryanmrichard <ryanmrichard1@gmail.com>
2018-02-14 16:38:28 -0600

    Starts rewrite and adds Space class

--- TensorWrapper/Space.cpp
+++ TensorWrapper/Space.cpp
@@ -7,11 +7,26 @@ using size_type = typename Space::size_type;
 using range_type = typename Space::range_type;
 
 size_type Space::size()const noexcept {
-    return  subspaces_.size() ?
-            std::accumulate(lengths_.begin(), lengths_.end(), 1L,
-            [](const size_type& total, const range_type& val){
-                return total*(val[1] - val[0]); //divided by val[2]
-            }) : 0L;
+    return  std::accumulate(lengths_.begin(), lengths_.end(),
+                            1L, std::multiplies<>());
 }
 
+bool Space::count_(const std::vector<size_type>& idx)const noexcept{
+    if(idx.size() > order()) return false;
+    for(size_type i=0; i < idx.size(); ++i)
+        if(idx[i] >= lengths_[i])
+            return false;
+    return true;
 }
+
+bool Space::operator==(const Space& rhs)const noexcept {
+    return lengths_  == rhs.lengths_;
+}
+
+bool Space::operator<(const Space& rhs)const noexcept {
+    return std::lexicographical_compare(lengths_.begin(), lengths_.end(),
+                                        rhs.lengths_.begin(), rhs.lengths_
+                                                .end());
+}
+
+} //End namespace
--- TensorWrapper_Test/TestSpace.cpp
+++ TensorWrapper_Test/TestSpace.cpp
@@ -1,5 +1,6 @@
 #include <TensorWrapper/Space.hpp>
 #include <catch/catch.hpp>
+#include <numeric>
 
 using namespace TensorWrapper;
 
@@ -7,72 +8,114 @@ template<std::size_t order>
 using array_type=std::array<std::size_t, order>;
 
 template<typename array_t>
-void check_state(const Space& s, const array_t& lows, const array_t& highs){
-    std::size_t size = 1;
-    for(std::size_t o=0; o<lows.size(); ++o)
-        size*=(highs[o] - lows[o]);
+void check_state(const Space& s, const array_t& lengths){
+    const std::size_t size = std::accumulate(lengths.begin(), lengths.end(),
+                                             1L, std::multiplies<>{});
+    const std::size_t order = lengths.size();
     REQUIRE(s.size() == size);
-    REQUIRE(s.order() == lows.size());
-    REQUIRE(s.nsubspaces() == 1);
-    REQUIRE(!s.empty());
-    array_t index(lows);
-    while(std::lexicographical_compare(index.begin(), index.end(),
-                                       highs.begin(), highs.end()))
-    {
-        REQUIRE(s.count(lows));
-        for(size_t i=1;i<=lows.size();++i)
-        {
-            const std::size_t idx = lows.size() - i;
-            if(index[idx] < highs[idx])
-            {
-                ++index[idx];
-                for(size_t j=1; j<i; ++j)
-                    index[idx+j] = lows[idx+j];
-            }
-
-        }
-    }
-    if(lows.size())
-    {
-        REQUIRE(!s.count(highs));
-        REQUIRE(!s.count(array_t(lows.size(), 99)));
-    }
-    else{
-        REQUIRE(s.count(lows));
-    }
-    index.push_back(99);
-    REQUIRE(!s.count(index));
+    REQUIRE(s.order() == order);
+    if(order)
+        REQUIRE(!s.count(lengths));
 }
 
-TEST_CASE("Empty")
-{
+TEST_CASE("Default Constructor"){
     Space s;
-    REQUIRE(s.size() == 0);
-    REQUIRE(s.order() == 0);
-    REQUIRE(s.nsubspaces() == 0);
-    REQUIRE(s.empty());
+    check_state(s, array_type<0>{});
 }
 
-TEST_CASE("Tensors"){
+
+TEST_CASE("Length Construction"){
     std::vector<std::string> names{"scalar", "vector", "matrix", "tensor"};
 
     /*
-     * Lows will be (0, 0, ...)
      * Highs will be (3, 5, 7 ...)
      */
 
     for(std::size_t i=0; i<4; ++i){
-        std::vector<std::size_t> lows(i);
         std::vector<std::size_t> highs(i);
         for(std::size_t j=0; j<i; ++j) highs[j]= 3 + 2*j;
-        SECTION("Full space for: " + names[i]){
+        SECTION(names[i] + " space"){
                 Space s{highs};
-                check_state(s, lows, highs);
-        }
-        for(std::size_t j=0; j<i; ++j)lows[j]=j+1;
-        SECTION("Sub space for: " + names[i]){
-            Space s{lows, highs};
-            check_state(s, lows, highs);
+                check_state(s, highs);
         }
     }
 }
+
+TEST_CASE("Copy/Move construction/assignment"){
+    array_type<2> lengths{3,4};
+    Space s{lengths};
+    SECTION("Copy construction"){
+        Space copy(s);
+        check_state(copy, lengths);
+    }
+    SECTION("Copy assignment"){
+        Space empty;
+        empty=s;
+        check_state(empty, lengths);
+    }
+    SECTION("Move construction"){
+        Space moved(std::move(s));
+        check_state(moved, lengths);
+    }
+    SECTION("Move assignment"){
+        Space empty;
+        empty = std::move(s);
+        check_state(empty, lengths);
+    }
+}
+
+//TEST_CASE("Relations") {
+//
+//    std::array<std::size_t, 2> lows{0, 0};
+//    std::array<std::size_t, 2> highs{3,3};
+//
+//    Space s{lows, highs};
+//
+//    SECTION("Self relations"){
+//        REQUIRE(s==s);
+//        REQUIRE(s<=s);
+//        REQUIRE(!(s<s));
+//        REQUIRE(!(s>s));
+//        REQUIRE(s>=s);
+//    }
+//
+//    SECTION("Empty relations"){
+//        Space empty;
+//        REQUIRE(empty != s);
+//        REQUIRE(empty < s);
+//        REQUIRE(empty <= s);
+//        REQUIRE(!(empty > s));
+//        REQUIRE(!(empty >= s));
+//    }
+//
+//    SECTION("Superspace") {
+//        highs[0]+=1;
+//        Space su{lows, highs};
+//        REQUIRE(s != su);
+//        REQUIRE(s <= su);
+//        REQUIRE(s <  su);
+//        REQUIRE(!(s> su));
+//        REQUIRE(!(s >= su));
+//    }
+//
+//    SECTION("Subspace") {
+//        highs[0]-=1;
+//        Space su{lows, highs};
+//        REQUIRE(s != su);
+//        REQUIRE(!(s <= su));
+//        REQUIRE(!(s <  su));
+//        REQUIRE(s> su);
+//        REQUIRE(s >= su);
+//    }
+//
+//    SECTION("Lower Order Subspace") {
+//        Space su{array_type<1>{0}, array_type<1>{3}};
+//        REQUIRE(s != su);
+//        REQUIRE(!(s <= su));
+//        REQUIRE(!(s <  su));
+//        REQUIRE(s> su);
+//        REQUIRE(s >= su);
+//    }
+//
+//
+//}
